771b06c2b2124ed5e1532ab8e96bde69fd0410d9
Tadas Audinis <tadas.audinis@vinted.com>
2024-01-27 12:46:15 +0200

    Add camera, furnace prefabs

--- Tractor League/Assets/Scripts/Camera/CameraZoomControl.cs
+++ Tractor League/Assets/Scripts/Camera/CameraZoomControl.cs
@@ -14,6 +14,7 @@ public class CameraZoomControl : MonoBehaviour
     private void Start()
     {
         cam = GetComponent<Camera>();
+        target = FindObjectOfType<Vehicle>().GetComponent<Rigidbody2D>();
     }
 
     private void Update()
