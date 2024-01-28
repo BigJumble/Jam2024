ec01d3981d24f91ae5f64515b3f55dbb7bbd6b95
Tadas Audinis <tadas.audinis@vinted.com>
2024-01-28 13:33:32 +0200

    Add cow sounds

--- Tractor League/Assets/Cow.cs
+++ Tractor League/Assets/Cow.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Animator))]
 public class Cow : MonoBehaviour
@@ -19,12 +20,19 @@ public class Cow : MonoBehaviour
     [SerializeField]
     private PolygonCollider2D polygonCollider;
 
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private List<AudioClip> mooSounds;
+
     private void Start()
     {
+        this.audioSource = GetComponent<AudioSource>();
         this.rb = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
     }
 
+    private bool wasRolling;
     private void FixedUpdate()
     {
         if(Mathf.Abs(transform.position.x) > 60 || Mathf.Abs(transform.position.y) > 60)
@@ -34,10 +42,13 @@ public class Cow : MonoBehaviour
             transform.position = Vector3.zero;
         }
 
+        wasRolling = rolling;
 
         rolling = Mathf.Abs(rb.angularVelocity) > spinAngularVelocityThreshold ||
             rb.velocity.magnitude > spinVelocityMagnitudeThreshold;
 
+        if(!wasRolling && rolling && !audioSource.isPlaying)
+            audioSource.PlayOneShot(mooSounds[Random.Range(0, mooSounds.Count)]);
 
         animator.SetBool("Rolling", rolling);
 
